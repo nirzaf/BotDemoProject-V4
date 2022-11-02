143552136ab1a6ade303ab5c68299e5ac7f1b68a
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-02 14:03:54 +0300

    Initial commit

---
+++ BookingDetails.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.18.1
+
+namespace BotDemoProject_V4;
+
+public class BookingDetails
+{
+    public string Destination { get; set; }
+
+    public string Origin { get; set; }
+
+    public string TravelDate { get; set; }
+}
